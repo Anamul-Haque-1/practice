736720ae3b03e58a305daf3c4c9c9e75b6657721
Anamul-Haque-1 <116352570+Anamul-Haque-1@users.noreply.github.com>
2024-02-18 09:35:59 +0000

    container config done

---
+++ test1.js
@@ -0,0 +1,12 @@
+const accountId=161625;
+let accountEmail="mdanamulhaque1996@gmail"
+var accountPassword="anamul"
+accountCity="Bogura";
+
+
+// accountId = 2;
+console.log(accountId);
+accountEmail="mdanamulhaque"
+accountPassword="Haque"
+accountCity="kajipur"
+console.table([accountEmail,accountId,accountPassword])
